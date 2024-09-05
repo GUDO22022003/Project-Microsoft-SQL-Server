dc38b28eb0eacdb97c54b8f7dc42d70ec77e2396
Hoàng Thông <91008461+GUDO22022003@users.noreply.github.com>
2024-09-05 16:02:27 +0700

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,127 @@
+﻿select * from Managers
+select * from Orders
+select * from Profiles
+select * from returns
+
+--- tính tổng doanh thu theo tháng năm và tháng 
+select year(order_date) as "năm" , month(order_date) as "tháng" , round(sum(value),0) as total_revenue from Orders
+group by year(order_date), month(order_date)
+order by "năm", "tháng"
+
+--- tính số đơn hàng phân theo phương thức vận chuyển --> Xem xét loại phương thức vận chuyển nào được ưu tiên nhất
+select shipping_mode as "phương thức vận chuyển", count(order_id) as "tổng đơn hàng" from Orders
+group by shipping_mode 
+order by "tổng đơn hàng" desc
+
+--- tính đơn hàng, doanh thu và chi phí theo province và region
+select province, region, count(order_id) as total_order, round(sum(value),0) as total_value, round(sum(profit),0) as total_profit from Orders
+group by province, region 
+order by province
+
+--- Request 2: Tỉ lệ trả hàng theo sản phẩm --> xác định loại sản phẩm nào có tỉ lệ trả hàng cao nhất
+	-- tính tổng số đơn hàng được đặt phân theo product_category
+	select product_category, count(order_id) as total_order into B from Orders
+	group by product_category
+	order by total_order desc
+		--tính tổng số đơn hàng trả lại theo product_category
+	select o.product_category, count(r.order_id) as total_order_return into A from orders as o
+	join returns as r
+	on r.order_id=o.order_id
+	group by o.product_category
+	-- Tính tỉ lệ đơn hàng trả lại 
+	select a.product_category, round(a.total_order_return*100/b.total_order,1,1) as return_rate from A as a
+	join B as b
+	on a.product_category = b.product_category
+	--> sản phẩm furniture có tỉ lệ trả hàng cao nhất: 11%
+
+--- Request 3:phân tích doanh thu theo mức ưu tiên đơn hàng --> xem mức ưu tiên có ảnh hưởng đến doanh thu hay không
+	-- Tính tổng doanh thu cho mỗi mức ưu tiên
+	select order_priority, round(sum(value),1,1) as total_revenue from orders
+	group by order_priority
+	order by total_revenue desc
+	---> mức ưu tiên là low thì có tổng doanh thu cao nhất 
+
+	
+--- Request 4: xác định khách hàng có doanh thu cao nhất theo vùng 
+	-- tính tổng doanh thu cho mỗi khách hàng 
+	WITH RankedCustomerRevenue AS (
+	SELECT Region, Customer_name, SUM(Value) AS CustomerRevenue,
+	ROW_NUMBER() OVER (PARTITION BY Region ORDER BY SUM(Value) DESC) AS Rank
+	FROM Orders
+	GROUP BY Region, Customer_name)
+	SELECT Region, Customer_name, CustomerRevenue
+	FROM RankedCustomerRevenue
+	order by region
+
+	select * from orders
+select * from returns
+
+--- request 5: tính tỉ lệ hoàn trả đơn hàng theo từng phân khúc
+
+With A as (select customer_segment, count(r.order_id) as total_Return from returns as r 
+join orders as o on o.order_id = r.order_id group by customer_segment), 
+B as (select customer_segment, count(order_id) as total_order from orders group by customer_segment)
+select a.Customer_segment, ROUND(CAST(total_Return AS DECIMAL(10,2)) * 100 / CAST(total_order AS DECIMAL(10,2)),2,1) as return_Rate from A left join B
+on a.customer_segment=b.customer_segment
+
+--- request 6: tính tỉ lệ hoàn trả đơn hàng theo mỗi vùng (region)
+with A as (select region, count(r.order_id) as total_return from returns as r join orders as o on o.order_id=r.order_id group by region), 
+B as (Select region, count(order_id) total_order from orders group by region)
+select a.region, round(cast(total_return as decimal(10,2))*100/cast(total_order as decimal (10,2)),2) as return_rate from A left join B
+on a.region = b.region
+
+
+--- Request 7: tìm sản phẩm có tổng số đơn hàng trả cao nhất trong mỗi phân khúc khách hàng 
+With A as (select o.customer_segment, o.product_name, count(r.order_id) as total_return from orders as o
+join returns as r 
+on o.order_id=r.order_id
+group by o.customer_segment, o.product_name) --- Tính tổng đơn hàng trả lại 
+select a.customer_segment, a.product_name, total_return from A
+inner join (
+    SELECT
+        Customer_segment,
+        MAX(total_return) AS MaxReturnedOrders
+    FROM A
+    GROUP BY Customer_segment
+) AS max_returned_orders 
+    ON a.Customer_segment = max_returned_orders.Customer_segment 
+    AND a.total_return = max_returned_orders.MaxReturnedOrders
+ORDER BY a.Customer_segment
+
+--- Request 7: tìm khách hàng có đơn hàng trả lại nhiều hơn mức trung bình của nhóm khách hàng có cùng phân khúc
+	With A as (select o.customer_name, o.customer_segment, count(r.order_id) as total_return from Orders as o 
+	left join Returns as r on r.order_id=o.order_id group by o.customer_name, o.customer_segment),
+	B as (select a.customer_segment, avg(total_return) as avg_return from A group by a.customer_segment)
+	select a.customer_name, a.customer_segment, a.total_return from A as a left join B as b 
+	on a.customer_segment=b.customer_segment
+	where total_return > avg_return
+
+--- Request 8: Tìm sản phẩm có tỉ lệ trả cao nhất của mỗi vùng 
+		With A as (select region, product_name, round(count(r.ordeR_id)*100/count(o.order_id),2,1) as return_rate
+	from Orders as o 
+	left join Returns as r on o.order_id=r.order_id group by region, product_name ) --- tìm vùng, tên sản phẩm, tỉ lệ trả đơn hàng
+	Select a.region, a.product_name, return_rate from A
+	inner join 
+	(
+	select region, max(return_rate) as max_return from A group by region
+	) as max_return_order
+	on a.region= max_return_order.region and a.return_rate= max_return_order.max_return
+	order by region asc --- tìm đơn sản phẩm của mỗi vùng có tỉ lệ trả cao nhất
+
+--- Request 9: Tính tổng lợi nhuận từ các đơn hàng được trả lại, phân loại theo ưu tiên của đơn hàng 
+	select o.order_priority, round(sum(profit),2,1) as total_profit from Orders as o left join Returns as r
+	on o.order_id = r.order_id
+	group by o.order_priority
+	order by total_profit desc
+
+--- Request 10: Tìm những đơn hàng được trả lại có giá trị cao nhất trong mỗi tháng
+	With A as (
+	select year(o.order_Date) as "năm", MONTH(o.order_date) as "tháng", o.order_id, round(sum(Value),0,1) as total_value from Orders as o 
+	inner join Returns as r on r.order_id=o.order_id
+	group by year(o.order_Date), MONTH(o.order_date), o.order_id
+	)
+	select a."năm", a."tháng", order_id, total_value from A 
+	inner join (select "năm", "tháng", max(total_value) as max_total_value from A group by "năm", "tháng") as max_value
+	on max_value."năm"=a."năm" and max_value."tháng"=a."tháng" and max_value.max_total_value=a.total_value
+	order by "năm", "tháng" asc
+	
---
+++ SQLQuery2.sql
@@ -0,0 +1,121 @@
+﻿select * from Orders
+select * from Returns
+select * from Managers
+select * from profiles
+
+
+select distinct shipping_mode from Orders
+
+--- Kết hợp 4 bảng
+--- Request 1: tìm vùng có hiệu suất kinh doanh tốt nhất --> Dựa trên total profit, total revenue và return rate
+	select o.region, m.manager_name, round(sum(o.profit),1,1) as total_profit, round(sum(value),1,1) as total_revenue, 
+	round(cast(count(r.order_id) as decimal(10,2))*100/cast(count(o.order_id) as decimal(10,2)),2,1) as return_rate
+	from Orders as o
+	left join Profiles as p on o.region=p.province
+	left join Returns as r on o.order_id=r.order_id
+	left join Managers as m on m.manager_name=p.manager
+	group by o.region, m.manager_name
+
+--- Request 2: Phân khúc khách hàng mang lại lợi nhuận nhiều nhất và ít trả lại hàng nhất 
+	select o.customer_segment, round(sum(o.profit),2,1) as total_profit, 
+	round(cast(count(r.order_id) as decimal(10,2))*100/cast(count(o.order_id) as decimal(10,2)),2,1) as return_rate
+	from Orders as o left join Returns as r on o.order_id=r.order_id
+	group by o.customer_segment
+	order by total_profit desc, return_rate asc
+
+--- Request 3: phương thức vận chuyển hiệu quả về cost và time
+	With ship as (select order_Date, shipping_date, shipping_mode, round(avg(shipping_cost),2,1) as avg_cost, 
+	datediff(day, orders.order_date,orders.shipping_date) as day_ship from Orders 
+	group by order_date, shipping_date, shipping_mode)
+	select ship.shipping_mode, avg_cost, day_ship from ship
+	inner join (
+	select ship.shipping_mode, min(ship.day_ship) as min_day_ship from ship
+	group by ship.shipping_mode
+	) as min_ship
+	on min_ship.shipping_mode=ship.shipping_mode and min_ship.min_day_ship=ship.day_ship 
+	inner join 
+	(
+	select ship.shipping_mode, min(ship.avg_cost) as min_cost from ship
+	group by ship.shipping_mode) as min_cost
+	on min_cost.min_cost=ship.avg_cost and min_cost.shipping_mode=ship.shipping_mode
+
+--- Request 4: tìm mối tương quan giữa chiết khấu và lợi nhuận 
+	With discount as (
+		select order_id, discount, profit,
+		case 
+		when discount < 0.05 then '0%-5%'
+		when discount between 0.05 and 0.1 then '5%-10%'
+		when discount between 0.1 and 0.15 then '10%-15%'
+		when discount > 0.15 then '>15%'
+		end as discount_rage
+		from Orders
+		group by order_id, discount, profit
+		)
+		select discount_rage, round(avg(profit),2,1) as avg_profit from discount
+		group by discount_rage
+
+--- Request 5: phân tích RFM (recency, frequency, monetary) để phân loại khách hàng 
+
+		With RFM as 
+		( 
+		select o.customer_name, datediff(day, max(o.order_Date), getdate()) as Rencency, count(o.order_id) as Frequency, sum(value) as Monetary from orders as o
+		group by o.customer_name
+		),	--- Recency: tính số ngày kể từ lần mua gần nhất 
+			--- Frequency: tính tổng số đơn hàng đặt bởi mỗi khách hàng 
+			--- Monetary: tính tổng giá trị đơn hàng của mỗi khách hàng
+		RFM_Score as (select cr.customer_name, cr.Rencency, cr.Frequency, cr.Monetary,
+		NTILE(5) over (order by cr.Rencency desc) as Rencency_Score,
+		NTILE(5) over (order by cr.Frequency desc) as Frequency_Score,
+		NTILE(5) over (order by cr.Monetary desc) as Monetary_Score
+		from RFM as cr) --- Phân loại khách hàng từ 1-5 trong RFM
+		select *, 
+		case
+		when rc.rencency_score = 5 and rc.Frequency_Score =5 and rc.Monetary_Score =5 then 'Champions'
+		when rc.rencency_score >=4 and rc.Frequency_Score >=4 then 'Loyal Customer'
+		when rc.rencency_score = 5 and rc.Frequency_Score = 1 then 'Pontential Loyalist'
+		when rc.rencency_score <=2 and rc.Frequency_Score >=3  then 'At Risk'
+		when rc.rencency_score <=2 and rc.Frequency_Score <=2  then 'Can not lose them'
+		else 'Lost'
+		End as Customer_segment
+		from RFM_Score as rc
+
+--- Request 6: phân tích cohort để đánh giá tỉ lệ giữ chân khách hàng 
+	With customer_cohort as 
+	( 
+	select customer_name, FIRST_VALUE(order_date) over (Partition by customer_name order by order_date asc) as Cohort_date from Orders 
+	),--- Xác định cohort của khách hàng dựa trên tháng họ đặt đơn hàng đầu tiên
+	Cohort_month as 
+	(
+	select cc.customer_name, cc.cohort_date, EOMONTH(o.order_date) as cohort_month
+	from customer_cohort as cc
+	inner join Orders as o on o.customer_name=cc.customer_name
+	) --- Xác định ngày&tháng cuối cùng mà khách hàng đặt hàng 
+	,
+	Cohort_retention as 
+	(
+	select cm.cohort_date, cm.cohort_month,
+	count(distinct cm.customer_name) as cohort_size,
+	count(distinct o.customer_name) as Returning_customer
+		from cohort_month as cm
+		left join Orders as o on o.customer_name=cm.customer_name and cm.cohort_month=EOMONTH(o.order_date)
+		group by cm.cohort_date, cm.cohort_month
+	) --- Xác định số lượng khách hàng duy nhất trong mỗi cohort và số lượng khách hàng quay lại trong mỗi cohort
+	select 
+	cr.cohort_date, 
+	cr.cohort_month,
+	cr.cohort_size,
+	cr.returning_customer,
+	round((cr.returning_customer*100)/cohort_size,0,1) as retention_rate
+	from Cohort_retention as cr --- Xác định tỉ lệ quay lại trong mỗi cohort
+
+--- Request 7: Phân tích mức độ hiệu quả của từng quản lí dựa trên số lượng đơn hàng và tỉ lệ trả lại 
+	select m.manager_name, m.manager_level,
+	count(distinct o.order_id) as total_order,
+	count(distinct r.order_id) as total_return,
+	round(count(distinct (cast(r.order_id as decimal(10,2))))*100/count(distinct (cast(o.order_id as decimal(10,2)))),1,1) as return_rate
+	from Orders as o
+	left join profiles as p on o.province=p.province
+	left join returns as r on o.order_id=r.order_id
+	left join managers as m on p.manager=m.manager_name
+	group by m.manager_name, m.manager_level
+	order by total_order desc
